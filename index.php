eb85560b42ce43120fcb4e04918d7c7dabe6e775
Lorena Arabia <56326571+Larabia@users.noreply.github.com>
2020-07-28 18:30:24 -0300

    adding variables to the story

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 function generateStory($singular_noun, $verb, $color){
-  $story = "\nThe woods are lovely,\ndark, and deep.
+  $story = "\nThe {$singular_noun}s are lovely,\n{$color}, and deep.
 \nBut I have promises to keep,
-\nAnd miles to go before I sleep,
-\nAnd miles to go before I sleep.\n";
+\nAnd miles to go before I {$verb},
+\nAnd miles to go before I {$verb}.\n";
 
 return $story;
 }
 
 echo generateStory("bed", "sleep", "red");
+
+echo generateStory("card", "take", "blue");
+
+echo generateStory("leaf", "shake", "green");
